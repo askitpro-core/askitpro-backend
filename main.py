0e6390367d1de39896195a3ac76c522b0f78da28
shivraj-09 <shivrajnirloti09@gmail.com>
2026-03-21 23:14:38 +0530

    Add POST /submit-doubt endpoint

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.routes import doubt_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(doubt_routes.router)
 
