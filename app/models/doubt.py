918c822ea33d0d8b9aa4ebae9a8352dedcf4941c
Dhiraj Reddy <25ca25.dhiraj@pccegoa.edu.in>
2026-03-21 21:26:44 +0530

    Update doubt.py

--- app/models/doubt.py
+++ app/models/doubt.py
@@ -1,5 +1,12 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class Doubt(BaseModel):
     title: str
     description: str
+    submitted_at: str = ""
+
+    def __init__(self, **data):
+        if "submitted_at" not in data:
+            data["submitted_at"] = str(datetime.now())
+        super().__init__(**data)
