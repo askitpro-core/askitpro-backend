04f3252a3d88d1bd33bdf070cc9559a99ca21ca0
shivraj-09 <shivrajnirloti09@gmail.com>
2026-03-27 22:59:31 +0530

    feat: upvote restriction (1 user = 1 vote) + vote model added

--- app/database.py
+++ app/database.py
@@ -3,8 +3,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///./askitpro.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
